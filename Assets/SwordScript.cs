5703222ad26a9b804273c34d3acd48501402c96e
Mihai <miuta.mihai@gmail.com>
2019-12-29 11:26:38 +0200

    Added explosion and collisions

---
+++ Assets/SwordScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour {
+	public GameObject explosion;
+
+	private void OnTriggerEnter2D(Collider2D collision){
+		if(collision.name == "Coin")
+		{
+			Debug.Log("am luat moneda");
+			GameObject e = Instantiate(explosion) as GameObject;
+			e.transform.position = collision.transform.position;
+			Destroy(collision.gameObject);
+		}
+	}
+}
